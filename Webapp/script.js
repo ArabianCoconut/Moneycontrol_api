54c5b7c367c011518ba069e2b3d316e7f5f46c07
ArabianCoconut <zwar73e7@duck.com>
2023-01-27 06:05:51 +0000

    Web app for Demo

---
+++ Webapp/script.js
@@ -0,0 +1,12 @@
+// Web version of python API
+
+// Global variables
+const url=["https://www.moneycontrol.com/news",
+"https://www.moneycontrol.com/news/business",
+"https://www.moneycontrol.com/news/latest-news/"]
+
+// Function to get the news
+function getNews(){
+    console.log("Getting news from "+url[0]);
+
+};
