69afce02fb7853b015f869a77d12181038e92957
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2023-07-26 15:48:07 +0530

    No change.

--- Test/Test_cases.py
+++ Test/Test_cases.py
@@ -4,5 +4,5 @@ import json
 test = mci.get_news()
 
 if __name__ == "__main__":
-    assert isinstance(json.loads(test),dict) != type(str), "Test Failed it should be json output in dict format"
+    assert isinstance(json.loads(test), dict) != type(str), "Test Failed it should be json output in dict format"
     print("Test passed")
