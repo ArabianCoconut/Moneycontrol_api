16ad94b11c5718abe7ffc36a1a6d25073f326ea9
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2023-07-26 15:47:00 +0530

    Fixed get_news() function and added version for requirements.txt

--- moneycontrol/moneycontrol_api.py
+++ moneycontrol/moneycontrol_api.py
@@ -1,20 +1,20 @@
 from bs4 import BeautifulSoup
-import requests,json
+import requests, json
 
 # Api for getting the latest news from moneycontrol.com
 
 # Constants
-title_text="Title:"
-link_text="Link:"
-date_text="Date:"
-html_parser="html.parser"
-news_type="News Type:"
-
+title_text = "Title:"
+link_text = "Link:"
+date_text = "Date:"
+html_parser = "html.parser"
+news_type = "News Type:"
 
 # Urls for getting the news
-url=["https://www.moneycontrol.com/news",
-"https://www.moneycontrol.com/news/business",
-"https://www.moneycontrol.com/news/latest-news/"]
+url = ["https://www.moneycontrol.com/news",
+       "https://www.moneycontrol.com/news/business",
+       "https://www.moneycontrol.com/news/latest-news/"]
+
 
 def get_news():
     """
@@ -27,16 +27,15 @@ def get_news():
     Returns:
     json_data (JSON object): A JSON object containing the title, link, and date of the news
     """
-    soup = BeautifulSoup(requests.get(url[0],timeout=60).text, html_parser)
-    # Get the title, link and date of the news
-    related_des_class = soup.find_all("h3", {"class": "related_des"})
-    related_date_class = soup.find_all("p", {"class": "related_date hide-mob"})
-    for h3_tag,p_tag in zip(related_des_class, related_date_class):
-        title = h3_tag.find("a").get("title")
-        link = h3_tag.find("a").get("href")
-        date = p_tag.text
-        json_data = {news_type:"News", title_text: title, link_text: link, date_text: date }
+    url_data = requests.get(url[0], timeout=60)
+    soup = BeautifulSoup(url_data.text, "html.parser")
+    soup_process = soup.find_all("h3", {"class": "related_des"})
+    for i in soup_process:
+        title_info = i.find("a").get("title")
+        link_info = i.find("a").get("href")
+        json_data = {news_type: "News", title_text: title_info, link_text: link_info}
         return json.dumps(json_data, indent=0)
+
 
 def get_business_news():
     """
@@ -49,25 +48,26 @@ def get_business_news():
     Returns:
     json_data (JSON object): A JSON object containing the title, link, and date of the news
     """
-    soup = BeautifulSoup(requests.get(url[1],timeout=60).text, html_parser)
+    soup = BeautifulSoup(requests.get(url[1], timeout=60).text, html_parser)
     # Get the title, link and date of the news
     for i in range(1, 24):
-        new_list = "newslist-"+str(i)
+        new_list = "newslist-" + str(i)
         news_list = soup.find("li", {"class": "clearfix", "id": new_list})
-        news_list_heading_2=soup.find("h1",{"class":"fleft"})
+        news_list_heading_2 = soup.find("h1", {"class": "fleft"})
         if news_list:
             title_class = news_list.find("h2").find("a")
             title = title_class.get("title")
             link = title_class.get("href")
             date_class = news_list.find("span")
             date = date_class.text
-            print(news_type,news_list_heading_2.text)
-            json_data = {news_type:news_list_heading_2.text ,
-            title_text: title, link_text: link, date_text: date }
+            print(news_type, news_list_heading_2.text)
+            json_data = {news_type: news_list_heading_2.text,
+                         title_text: title, link_text: link, date_text: date}
             return json.dumps(json_data, indent=0)
         else:
             print(f"li element with class 'clearfix' and id '{id}' not found.")
             return None
+
 
 def get_latest_news():
     """
@@ -80,13 +80,16 @@ def get_latest_news():
     Returns:
     json_data (JSON object): A JSON object containing the title, link, and date of the news
     """
-    soup = BeautifulSoup(requests.get(url[2],timeout=60).text, html_parser)
+    soup = BeautifulSoup(requests.get(url[2], timeout=60).text, html_parser)
     # Get the title, link and date of the news
     related_des_class = soup.find_all("h3", {"class": "related_des"})
     related_date_class = soup.find_all("p", {"class": "related_date hide-mob"})
-    for h3_tag,p_tag in zip(related_des_class, related_date_class):
+    for h3_tag, p_tag in zip(related_des_class, related_date_class):
         title = h3_tag.find("a").get("title")
         link = h3_tag.find("a").get("href")
         date = p_tag.text
-        json_data = {news_type:"Latest News", title_text: title, link_text: link, date_text: date}
+        json_data = {news_type: "Latest News", title_text: title, link_text: link, date_text: date}
         return json.dumps(json_data, indent=0)
+
+# # get_business_news()
+# # get_latest_news()
