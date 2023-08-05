7496e497373f4ded2836b3212c65af4445e1f1d1
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2023-08-05 15:45:50 +0530

    Completed API server

--- Src/app.py
+++ Src/app.py
@@ -1,11 +1,11 @@
-# import moneycontrol.moneycontrol_api as mc
+import moneycontrol.moneycontrol_api as mc
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/api', methods=['POST'])
-def api():
+@app.route('/api/<news>', methods=['GET'])
+def api(news):
     """
     Gets the news from the given URL and returns a JSON object containing the title, link,
     and date of the news.
@@ -13,17 +13,22 @@ def api():
     Parameters:
     url (string): The URL from which to retrieve the news
 
-    Returns:
-    json_data (JSON object): A JSON object containing the title, link, and date of the news
+    Returns: json_data (JSON object): A JSON object containing the title, link, and date of the news, business news,
+    and latest news.
     """
+    if request.method == 'GET' and news == 'news':
+        return jsonify(mc.get_news())
+    elif request.method == 'GET' and news == 'business':
+        return jsonify(mc.get_business_news())
+    elif request.method == 'GET' and news == 'latest':
+        return jsonify(mc.get_latest_news())
+    else:
+        return jsonify({"error": "Method not allowed"})
 
-
-# TODO: Add the ability to get news and return JSON.
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
