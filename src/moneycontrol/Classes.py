5db7ad13330293cdf1ba2698cf13cdb12620d420
ArabianCoconut <zwar73e7@duck.com>
2024-05-16 14:12:09 +0000

    Refactor code to move classes to dedicated Class file and fix issues with Business news API

--- src/moneycontrol/Classes.py
+++ src/moneycontrol/Classes.py
@@ -1,9 +1,11 @@
 import datetime
 import json
 import logging
-from pymongo import MongoClient, errors
-from dotenv import load_dotenv,find_dotenv
 from os import environ as env
+
+from dotenv import find_dotenv, load_dotenv
+from pymongo import MongoClient, errors
+
 
 class Api:
     """
@@ -24,7 +26,6 @@ class Api:
         ]
 
 
-
 class StorageController:
     def __init__(self):
         """Summary
@@ -37,7 +38,9 @@ class StorageController:
         self.DB_COLLECTION = env.get("DB_COLLECTION")
         self.DB_LOGIN = env.get("DB_LOGIN")
         self.client = MongoClient(self.DB_LOGIN)
-        self.db = self.client.get_database(self.DB_NAME).get_collection(self.DB_COLLECTION)
+        self.db = self.client.get_database(self.DB_NAME).get_collection(
+            self.DB_COLLECTION
+        )
 
     def connect_to_db(self):
         """Summary
@@ -48,7 +51,9 @@ class StorageController:
         """
         try:
             self.client = MongoClient(self.DB_LOGIN)
-            self.db = self.client.get_database(self.DB_NAME).get_collection(self.DB_COLLECTION)
+            self.db = self.client.get_database(self.DB_NAME).get_collection(
+                self.DB_COLLECTION
+            )
             return self.db
         except Exception as e:
             print("Error in connecting to database. Error:", e)
@@ -65,21 +70,28 @@ class StorageController:
         json_data (str): The data in JSON format
         """
         try:
-            json_data = json.dumps(list(self.connect_to_db().find(filters)), indent=2, default=str)
+            json_data = json.dumps(
+                list(self.connect_to_db().find(filters)), indent=2, default=str
+            )
             if json_data == "[]":
                 return json.dumps(
                     {
                         "status": "No data found in the database.",
-                        "suggestion": "Please insert some data into the database using the /api/news endpoint."
-                    }, indent=2, default=str)
+                        "suggestion": "Please insert some data into the database using the /api/news endpoint.",
+                    },
+                    indent=2,
+                    default=str,
+                )
             elif filters is None:
-                return json.dumps(list(self.connect_to_db().find()), indent=2, default=str)
+                return json.dumps(
+                    list(self.connect_to_db().find()), indent=2, default=str
+                )
             else:
                 return json_data
         except errors as e:
             return ("Error:", e)
 
-    def insert_data_to_db(self,data, filters=None):
+    def insert_data_to_db(self, data, filters=None):
         """
         Inserts the given data into the database and also returns the inserted data
 
@@ -105,6 +117,3 @@ class StorageController:
         except errors.DuplicateKeyError as e:
             logging.warning(f"Data already exists in the database. Error: {str(e)}")
             return self.dump_all_data_to_json(filters)
-
-
-Api().StorageController.connect_to_db().delete_many({})
